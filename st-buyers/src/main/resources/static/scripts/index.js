194537fc4b6adf2735b921ae2a18c196b6e7f929
zhibin.zhao <525904821@qq.com>
2017-06-01 16:09:05 +0800

    新增界面

--- st-buyers/src/main/resources/static/scripts/index.js
+++ st-buyers/src/main/resources/static/scripts/index.js
@@ -2,13 +2,14 @@
  * Created by Administrator on 2017/5/10.
  */
 $(function () {
-    $(".container .footer ul li").click(function () {
+    /*$(".container .footer ul li").click(function () {
         var $footer = $(".container .footer ul li");
         if(!$(this).hasClass("active")){
             $footer.each(function () {
                 $(this).removeClass("active");
             });
+            location.href = "http://www.baidu.com";
             $(this).addClass("active");
         }
-    });
+    });*/
 })
