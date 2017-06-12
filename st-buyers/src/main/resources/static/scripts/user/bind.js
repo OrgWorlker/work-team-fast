6a5480b7d225026e0ff957114d25c4f501f2e31d
zhicong.lin <zhicong.lin@qishon.com>
2017-06-12 15:06:39 +0800

    增加数据

--- st-buyers/src/main/resources/static/scripts/user/bind.js
+++ st-buyers/src/main/resources/static/scripts/user/bind.js
@@ -7,7 +7,7 @@ $(function () {
     var selectAge = new MobileSelectArea();
     selectAge.init({level: 1,trigger:$('#text_age'),value:$('#hd_age').val(),data:'/static/json/age.json'});
     var selectLevel = new MobileSelectArea();
-    selectLevel.init({level: 1,trigger:$('#text_level'),value:$('#hd_level').val(),data:'/static/json/age.json'});
+    selectLevel.init({level: 1,trigger:$('#text_level'),value:$('#hd_level').val(),data:'/static/json/level.json'});
     // var selectLabel = new MobileSelectArea();
     // selectLabel.init({level: 1,trigger:$('#text_label'),value:$('#hd_label').val(),data:'/static/json/age.json'});
     /*$("#tijiao").click(function () {
